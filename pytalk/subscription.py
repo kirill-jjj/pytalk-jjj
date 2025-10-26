b3d97be06d480830f6c441656111588e2242c514
kirill-jjj <kirillkolovi@gmail.com>
2025-10-26 15:11:21 +0500

    fix(subscription): Correctly map USER_TEXTMESSAGE in _SubscriptionMeta.__getattr__

--- pytalk/subscription.py
+++ pytalk/subscription.py
@@ -7,7 +7,10 @@ from .implementation.TeamTalkPy import TeamTalk5 as sdk
 
 class _SubscriptionMeta(type):
     def __getattr__(cls, name: str) -> sdk.UserRight:
-        name = f"SUBSCRIBE_{name}"
+        if name == "USER_TEXTMESSAGE":
+            name = "SUBSCRIBE_USER_MSG"
+        else:
+            name = f"SUBSCRIBE_{name}"
         return cast("sdk.UserRight", getattr(sdk.Subscription, name, None))
 
     def __dir__(cls) -> list[str]:
