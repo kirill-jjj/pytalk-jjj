b2a501b39aa986c418087102d83708394dfd57d3
kirill-jjj <79443292+kirill-jjj@users.noreply.github.com>
2025-10-29 22:14:48 +0500

    🐛 fix(ruff): Fix ruff errors in tools
    Co-authored-by: BlindMaster24 <375291171150z@gmail.com>

--- pytalk/tools/downloader.py
+++ pytalk/tools/downloader.py
@@ -1,15 +1,19 @@
-# Modified from https://github.com/gumerov-amir/TTMediaBot
-
+"""Utility functions for downloading files."""
 
 import shutil
+from pathlib import Path
 
 import requests
 
 
 def download_file(url: str, file_path: str) -> None:
+    """Download a file from a given URL to a specified path."""
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
     }
-    with requests.get(url, headers=headers, stream=True) as r:
-        with open(file_path, "wb") as f:
-            shutil.copyfileobj(r.raw, f)
+    with (
+        requests.get(url, headers=headers, stream=True, timeout=10) as r,
+        Path(file_path).open("wb") as f,
+    ):
+        shutil.copyfileobj(r.raw, f)
